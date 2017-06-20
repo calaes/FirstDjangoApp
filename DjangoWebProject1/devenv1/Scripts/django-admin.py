87d007c1d179e498758653835f36cf367d914ade
calaes <calaes@mtu.edu>
2017-06-19 19:58:43 -0500

    First Merge

---
+++ DjangoWebProject1/devenv1/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\calaes\documents\visual studio 2017\Projects\DjangoWebProject1\DjangoWebProject1\devenv1\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
